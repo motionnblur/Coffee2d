be41010be5329593f21f64f5a3c81244d413e183
chill <chill@mail.com>
2023-12-06 17:08:49 +0300

    added

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "lab.cpp"
+
+int main()
+{
+    InitEngine();
+    InitGlfw();
+    InitWindow(800, 600, "MESA");
+    InitOpengl();
+
+    MainLoop();
+
+    QuitOpengl();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
